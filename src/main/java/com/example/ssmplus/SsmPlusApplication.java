4a61a02f959c4dab100fdff057832db75d465c6f
godrhyme <984983605@qq.com>
2020-09-09 15:20:59 +0800

    mybatis-plus模板配置

---
+++ src/main/java/com/example/ssmplus/SsmPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssmplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmPlusApplication.class, args);
+    }
+
+}
